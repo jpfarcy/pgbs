b7bc6e9d35195b07d8ab4de6ffcfc4d030261d93
jp <jp@Endor.local>
2013-01-30 22:09:59 +0100

    bug fix

--- install_tpl.sh
+++ install_tpl.sh
@@ -66,8 +66,8 @@ fi
 read -p "Choose lang of PGBS default config file [fr]:" lang
 lang=${lang:=fr}
 # Backup existing conf
-if [[ -L $rootdir/cfg/pgbackup.config]]; then
-	cfgfile=$(ls -l $rootdir/cfg/pgbackup.config|cut -d-> f2)
+if [[ -L $rootdir/cfg/pgbackup.config ]]; then
+	cfgfile=$(ls -l $rootdir/cfg/pgbackup.config|cut -d '>' -f2)
 	mv $rootdir/cfg/$cfgfile $rootdir/cfg/pgbackup.config.bkp
 fi
 # Install
